4729c41264096893393d3bbe9b69b2a90c21a1a3
Javier Arias <parias@heinsohn.com.co>
2024-05-28 00:13:14 -0500

    Se ajusta estilos en grupos, asociar docentes y espacios fisicos en planificación

--- src/app/componentes/planificacion-horario/gestion-grupos/components/asignar-espacio-fisico/asignar-espacio-fisico.component.ts
+++ src/app/componentes/planificacion-horario/gestion-grupos/components/asignar-espacio-fisico/asignar-espacio-fisico.component.ts
@@ -7,125 +7,126 @@ import { AgrupadorEspacioFiscioDTO } from 'src/app/shared/model/AgrupadorEspacio
 import { GrupoService } from '../../services/grupo.service';
 import { ShowMessageService } from 'src/app/shared/service/show-message.service';
 import { EspacioFisicoServicio } from 'src/app/componentes/servicios/espacio.fisico.servicio';
+import { UbicacionOutDTO } from 'src/app/componentes/dto/espacio-fisico/out/ubicacion.out.dto';
 
 @Component({
-  selector: 'app-asignar-espacio-fisico',
-  templateUrl: './asignar-espacio-fisico.component.html',
-  styleUrls: ['./asignar-espacio-fisico.component.scss']
+selector: 'app-asignar-espacio-fisico',
+templateUrl: './asignar-espacio-fisico.component.html',
+styleUrls: ['./asignar-espacio-fisico.component.scss']
 })
 export class AsignarEspacioFisicoComponent implements OnInit {
 
-  grupoSeleccionado: AgrupadorEspacioFiscioDTO
+    grupoSeleccionado: AgrupadorEspacioFiscioDTO
 
-  listaTipoEspacioFisico: TipoEspacioFisicoOutDTO[] = []
-  
-  espaciosFisicosDisponibles: any[] = []
-  espasciosFisicosAsignados: any[] = []
+    listaTipoEspacioFisico: TipoEspacioFisicoOutDTO[] = []
 
-  listaDeGruposNuevos: any = []
-  listaDeGruposQuitados: any = []
-  lstUbicacion: string[] = [];
+    espaciosFisicosDisponibles: any[] = []
+    espasciosFisicosAsignados: any[] = []
 
-  filtro: any = {
-    ubicacion: "",
-    tipo: "",
-    nombre: "",
-    idAgrupador: null
-  } as any
+    listaDeGruposNuevos: any = []
+    listaDeGruposQuitados: any = []
+    lstUbicacion: UbicacionOutDTO[] = [];
 
-  constructor(private config: DynamicDialogConfig,
-    private grupoService: GrupoService,
-    private ref: DynamicDialogRef,
-    private mensajeService: ShowMessageService,
-    private espacioFisicoServicio: EspacioFisicoServicio
-  ) {}
-  ngOnInit(): void {
-    this.obtenerUbicaciones()
-    this.grupoSeleccionado = this.config.data?.grupo
-    this.grupoService.obtenerEspacioFiscioAgrupador(this.grupoSeleccionado.idAgrupadorEspacioFisico).subscribe({
-      next: (r) =>
-        this.espasciosFisicosAsignados = r
-    })
-    this.filtrarEspacios()
-  }
+    filtro: any = {
+        ubicacion: "",
+        tipo: "",
+        nombre: "",
+        idAgrupador: null
+    } as any
 
-  obtenerTipos() {
-    let ubicaciones: string[] = []
-    ubicaciones.push(this.filtro.ubicacion)
-    /*this.espacioFisicoServicio.consultarTiposEspaciosFisicosPorUbicaciones(ubicaciones).subscribe(r => {
-      this.listaTipoEspacioFisico = r
-    })*/
-  }
+    constructor(private config: DynamicDialogConfig,
+        private grupoService: GrupoService,
+        private ref: DynamicDialogRef,
+        private mensajeService: ShowMessageService,
+        private espacioFisicoServicio: EspacioFisicoServicio
+    ) {}
 
-  obtenerUbicaciones() {
-    /*this.espacioFisicoServicio.consultarUbicaciones().subscribe(
-      (lstUbicacion: string[]) => {
-          this.lstUbicacion = lstUbicacion;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );*/
-  }
-
-  inicializarFormulario() {
-  }
-
-  filtrarEspacios() {
-    this.filtro.idAgrupador = this.grupoSeleccionado.idAgrupadorEspacioFisico
-    this.grupoService.obtenerEspacioFiscioDispinibleFiltro(this.filtro).subscribe({
-      next: (r) => {
-        this.espaciosFisicosDisponibles = r
-        this.espaciosFisicosDisponibles = this.espaciosFisicosDisponibles.concat(this.listaDeGruposQuitados)
-        this.espaciosFisicosDisponibles = this.quitarRepetidos(this.espaciosFisicosDisponibles)
-      }
-    })
-  }
-
-  onChangeUbicacion() {
-    if (this.filtro.ubicacion) {
-      this.obtenerTipos()
-      this.filtro.tipo = ""
-    } else {
-      this.filtro.ubicacion = ""
-      this.filtro.tipo = ""
+    ngOnInit(): void {
+        this.obtenerUbicaciones()
+        this.grupoSeleccionado = this.config.data?.grupo
+        this.grupoService.obtenerEspacioFiscioAgrupador(this.grupoSeleccionado.idAgrupadorEspacioFisico).subscribe({
+        next: (r) =>
+            this.espasciosFisicosAsignados = r
+        })
+        this.filtrarEspacios()
     }
-    this.filtrarEspacios()
-  }
-  guardar() {
-    const asignacionDTO = {
-      quitados: this.listaDeGruposQuitados,
-      agregados: this.listaDeGruposNuevos,
-      idGrupo: this.grupoSeleccionado.idAgrupadorEspacioFisico
+
+    obtenerTipos() {
+        let ubicaciones: number[] = []
+        ubicaciones.push(this.filtro.ubicacion)
+        this.espacioFisicoServicio.consultarTiposEspaciosFisicosPorUbicaciones(ubicaciones).subscribe(r => {
+        this.listaTipoEspacioFisico = r
+        })
     }
-    this.grupoService.guardarAsignacion(asignacionDTO).subscribe(r => {
-      if (r && r.error) {
-        this.mensajeService.showMessage("error", r.descripcion)
-      } else {
-        this.mensajeService.showMessage("success", r.descripcion)
-      }
-      this.ref.close()
-    })
-  }
-  salir() {
-    this.ref.close()
-  }
-  handleMoveToTarget($event: any) {
-    this.listaDeGruposNuevos = this.listaDeGruposNuevos.concat($event.items)
-    this.listaDeGruposNuevos = this.quitarRepetidos(this.listaDeGruposNuevos);
-  }
-  handleMoveToSource($event: any) {
-    this.listaDeGruposQuitados = this.listaDeGruposQuitados.concat($event.items)
-    this.listaDeGruposQuitados = this.quitarRepetidos(this.listaDeGruposQuitados);
-  }
-  
-  quitarRepetidos(lista: any[]): any[] {
-    const map = new Map();
-    lista.forEach(salon => {
-        map.set(salon.idEspacioFisico, salon);
-    });
 
-    return Array.from(map.values());
-  }
+    obtenerUbicaciones() {
+        this.espacioFisicoServicio.consultarUbicaciones().subscribe(
+        (lstUbicacion: UbicacionOutDTO[]) => {
+            this.lstUbicacion = lstUbicacion;
+        },
+        (error) => {
+            console.error(error);
+        }
+        );
+    }
 
+    inicializarFormulario() {
+    }
+
+    filtrarEspacios() {
+        this.filtro.idAgrupador = this.grupoSeleccionado.idAgrupadorEspacioFisico
+        this.grupoService.obtenerEspacioFiscioDispinibleFiltro(this.filtro).subscribe({
+        next: (r) => {
+            this.espaciosFisicosDisponibles = r
+            this.espaciosFisicosDisponibles = this.espaciosFisicosDisponibles.concat(this.listaDeGruposQuitados)
+            this.espaciosFisicosDisponibles = this.quitarRepetidos(this.espaciosFisicosDisponibles)
+        }
+        })
+    }
+
+    onChangeUbicacion() {
+        if (this.filtro.ubicacion) {
+        this.obtenerTipos()
+        this.filtro.tipo = ""
+        } else {
+        this.filtro.ubicacion = ""
+        this.filtro.tipo = ""
+        }
+        this.filtrarEspacios()
+    }
+    guardar() {
+        const asignacionDTO = {
+        quitados: this.listaDeGruposQuitados,
+        agregados: this.listaDeGruposNuevos,
+        idGrupo: this.grupoSeleccionado.idAgrupadorEspacioFisico
+        }
+        this.grupoService.guardarAsignacion(asignacionDTO).subscribe(r => {
+        if (r && r.error) {
+            this.mensajeService.showMessage("error", r.descripcion)
+        } else {
+            this.mensajeService.showMessage("success", r.descripcion)
+        }
+        this.ref.close()
+        })
+    }
+    salir() {
+        this.ref.close()
+    }
+    handleMoveToTarget($event: any) {
+        this.listaDeGruposNuevos = this.listaDeGruposNuevos.concat($event.items)
+        this.listaDeGruposNuevos = this.quitarRepetidos(this.listaDeGruposNuevos);
+    }
+    handleMoveToSource($event: any) {
+        this.listaDeGruposQuitados = this.listaDeGruposQuitados.concat($event.items)
+        this.listaDeGruposQuitados = this.quitarRepetidos(this.listaDeGruposQuitados);
+    }
+
+    quitarRepetidos(lista: any[]): any[] {
+        const map = new Map();
+        lista.forEach(salon => {
+            map.set(salon.idEspacioFisico, salon);
+        });
+
+        return Array.from(map.values());
+    }
 }
