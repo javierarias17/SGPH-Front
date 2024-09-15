7c852a0e5827fbf2aa5cb261f7f766d95bdc917c
Javier Arias <apedro@unicauca.edu.co>
2024-09-15 08:25:48 -0500

    Se cambia ruta enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //url: "http://159.89.37.197:8080/"
-  url: "http://localhost:8080/"
+  url: "http://159.89.37.197:8080/"
+  //url: "http://localhost:8080/"
 
 };
