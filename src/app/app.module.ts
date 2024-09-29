e57c615b4401a55c0e152d0a331fb27c2152600f
Javier Arias <parias@heinsohn.com.co>
2024-09-29 16:13:50 -0500

    Se configura idioma español y se ajusta error de calendar en campo fecha inicio en editar periodo academico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
@@ -14,7 +14,7 @@ import { PhotoService } from './demo/service/photo.service';
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ConfirmationService } from 'primeng/api';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
 import { PeriodoAcademicoService } from './shared/service/periodo.academico.service';
 import { LenguajeService } from './componentes/servicios/lenguaje.service';
 import { OAuthModule } from 'angular-oauth2-oidc';
@@ -22,9 +22,16 @@ import { SpinnerService } from './shared/service/spinner.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './auth.interceptor';
 
+// Se importa registerLocaleData y los datos de localización en español
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
+
+// Registramos los datos de localización para español
+registerLocaleData(localeEs);
 
 @NgModule({
     declarations: [
@@ -45,6 +52,8 @@ export function HttpLoaderFactory(http: HttpClient) {
         SharedModule
     ],
     providers: [
+        // Proveemos LOCALE_ID con el valor 'es' para establecer el idioma español
+        { provide: LOCALE_ID, useValue: 'es' },
         SpinnerService,
         CountryService, CustomerService, EventService, IconService, NodeService,
         PhotoService, ProductService, LenguajeService, ConfirmationService, PeriodoAcademicoService,
@@ -58,6 +67,28 @@ export function HttpLoaderFactory(http: HttpClient) {
 })
 export class AppModule {
     /*Se inyecta para configurar el archivo de internacionalización*/
-    constructor(private languageService: LenguajeService) {
+    constructor(private languageService: LenguajeService, private primengConfig: PrimeNGConfig) {
+
+        // Se configura en español el calendario de los formularios
+        this.primengConfig.setTranslation({
+            dayNames: [
+              "domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"
+            ],
+            dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+            dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+            monthNames: [
+              "enero", "febrero", "marzo", "abril", "mayo", "junio",
+              "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+            ],
+            monthNamesShort: [
+              "ene", "feb", "mar", "abr", "may", "jun",
+              "jul", "ago", "sep", "oct", "nov", "dic"
+            ],
+            today: "Hoy",
+            clear: "Limpiar",
+            dateFormat: "dd/mm/yy",
+            weekHeader: "Sem",
+            firstDayOfWeek: 1,
+          });
     }
  }
