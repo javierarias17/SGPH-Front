5f1b24a007cf911137d29ceeedde7b920b89fa6b
Javier Arias <parias@heinsohn.com.co>
2024-11-13 22:40:07 -0500

    Se elimina componente repetido de cargar.labor.docencia

--- src/app/componentes/datos/cargar-labor-docencia/pages/cargar.labor.docencia.component.ts
+++ src/app/componentes/datos/cargar-labor-docencia/pages/cargar.labor.docencia.component.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ProgramaService } from '../../../common/services/programa.service';
 import { ProgramaOutDTO } from '../../../common/model/programa/out/programa.out.dto';
 import { SharedService } from 'src/app/shared/service/shared.service';
-import { VisualizadorExcelComponent } from 'src/app/shared/components/visualizador-excel/visualizador-excel.component';
-import { DialogService } from 'primeng/dynamicdialog';
-import { archivoBase64 } from 'src/app/shared/Constantes';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ShowMessageService } from 'src/app/shared/service/show-message.service';
 import { PeriodoAcademicoSharedService } from 'src/app/shared/service/periodo.academico.shared.service';
 import { PeriodoAcademicoOutDTO } from 'src/app/componentes/periodo-academico/gestionar-periodo-academico/model/out/periodo-academico-out-dto';
 import { Message } from 'primeng/api';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { InformacionDetalleCargueComponent } from '../components/informacion-detalle-cargue/informacion-detalle-cargue.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cargar-labor-docencia',
   templateUrl: './cargar.labor.docencia.component.html',
-  styleUrls: ['./cargar.labor.docencia.component.css']
+  styleUrls: ['./cargar.labor.docencia.component.css'],
+  providers: [DialogService],
 })
 export class CargarLaborDocenciaComponent implements OnInit {
 
@@ -24,7 +25,8 @@ export class CargarLaborDocenciaComponent implements OnInit {
 	eliminar: boolean = false
 	listaProgramas: ProgramaOutDTO[]
 	programa: ProgramaOutDTO
-	base64: number
+	isLoading: boolean = false;
+	ref: DynamicDialogRef;
 
 	public mensajesError:string = "";
 	public messages: Message[] = null;
@@ -34,7 +36,8 @@ export class CargarLaborDocenciaComponent implements OnInit {
 		private sharedService: SharedService,
 		private dialogService: DialogService,
 		private messageService: ShowMessageService,
-		public periodoAcademicoSharedService:PeriodoAcademicoSharedService
+		public periodoAcademicoSharedService:PeriodoAcademicoSharedService,
+		private http: HttpClient
 	) {
 
 	}
@@ -72,50 +75,6 @@ export class CargarLaborDocenciaComponent implements OnInit {
 	private limpiar() {
         this.formulario.reset();
     }
-	
-	public guardar() {
-		if (!this.formulario.valid) {
-			this.formulario.markAllAsTouched();
-			return;
-		}
-		// MOCK BASE 64
-		this.sharedService.cargarLaborDocente({
-		idPrograma: this.idPrograma().value,
-		archivoBase64: archivoBase64
-		}).subscribe(r => {
-		if (r && r.error) {
-			this.mensajesError = r.descripcion;
-			this.messageService.showMessage("error", r.descripcion)
-		} else {
-			this.messageService.showMessage("success", r.descripcion)
-		}
-		})	
-	}
-
-	public visualizar() {
-		if (!this.formulario.valid) {
-			this.formulario.markAllAsTouched();
-			return ;
-		}
-		// MOCK BASE64
-		this.sharedService.obtenerVisualizarLaborDocente({
-		idPrograma: this.idPrograma().value,
-		archivoBase64: archivoBase64
-		}).subscribe(r => {
-			if (r) {
-				this.base64 = r.archivoBase64
-				this.dialogService.open(VisualizadorExcelComponent, {
-				height: '90vh',
-				width: '95%',
-				header: 'Visualizar labor docente',
-				contentStyle: { 'overflow': 'hidden' },
-				data: {
-					base64: this.base64
-				}
-				});
-			}
-		})		
-	}
 
   	private consultarPeriodoAcademicoVigente():void{
 		this.periodoAcademicoSharedService.consultarPeriodoAcademicoVigente().subscribe(
@@ -131,4 +90,130 @@ export class CargarLaborDocenciaComponent implements OnInit {
 			}
 		);        
 	}
+
+	guardar() {
+        this.isLoading = true;
+        this.http.get<any[]>('/assets/mock-labor-docente.json').subscribe(
+            (docenteLaborList) => {
+                this.sharedService
+                    .importarLaborDocente(
+                        docenteLaborList,
+                        1,
+                        this.idPrograma().value
+                    )
+                    .subscribe(
+                        (response) => {
+                            this.isLoading = false;
+                            this.messageService.showMessage(
+                                'success',
+                                'Labor docente importada correctamente'
+                            );
+
+                            // Abre el modal con estado de éxito
+                            this.ref = this.dialogService.open(
+                                InformacionDetalleCargueComponent,
+                                {
+                                    header: 'Información detalle cargue',
+                                    data: {
+                                        facultad:
+                                            'Facultad de Ingeniería Electrónica y Telecomunicaciones',
+                                        programa: "Nombre del programa",
+                                        estado: 'Cargado con éxito',
+                                        detalleCargue: response,
+                                    },
+                                    width: '50%',
+                                }
+                            );
+                        },
+                        (error) => {
+                            this.isLoading = false;
+                            console.error('Error al importar:', error);
+
+                            // Configura el mensaje de error
+                            let mensajeError =
+                                'Ocurrió un error inesperado al importar la labor docente.';
+                            if (error.error && Array.isArray(error.error)) {
+                                mensajeError = error.error.join('\n');
+                            } else if (typeof error.error === 'string') {
+                                mensajeError = error.error;
+                            }
+
+                            // Muestra el modal con estado de fallo
+                            this.ref = this.dialogService.open(
+                                InformacionDetalleCargueComponent,
+                                {
+                                    header: 'Información detalle cargue',
+                                    data: {
+                                        facultad:
+                                            'Facultad de Ingeniería Electrónica y Telecomunicaciones',
+                                        estado: 'Fallo cargue',
+                                        detalleCargue: [mensajeError], // Muestra el mensaje de error en detalleCargue
+                                    },
+                                    width: '50%',
+                                }
+                            );
+                        }
+                    );
+            },
+            (error) => {
+                this.isLoading = false;
+                console.error('Error al cargar el archivo JSON:', error);
+                this.messageService.showMessage(
+                    'error',
+                    'Error al cargar el archivo JSON de prueba.'
+                );
+            }
+        );
+    }
+
+    visualizar() {
+        if (this.programa) {
+            const filtro = {
+                idPrograma: this.programa.idPrograma,
+                nombrePrograma: this.programa.nombre,
+            };
+
+            this.sharedService.obtenerVisualizarLaborDocente(filtro).subscribe(
+                (response) => {
+                    console.log('Respuesta de visualizar:', response);
+                    this.messageService.showMessage(
+                        'success',
+                        'Visualización de la labor docente realizada correctamente'
+                    );
+                },
+                (error) => {
+                    console.error('Error al visualizar:', error);
+                    this.messageService.showMessage(
+                        'error',
+                        'Error al visualizar la labor docente'
+                    );
+                }
+            );
+        } else {
+            this.messageService.showMessage(
+                'error',
+                'Por favor, seleccione un programa'
+            );
+        }
+    }
+
+    mostrarInformacionDetalleCargue() {
+        this.ref = this.dialogService.open(InformacionDetalleCargueComponent, {
+            header: 'Información detalle cargue - Telemática',
+            width: '50%',
+            data: {
+                facultad:
+                    'Facultad de Ingeniería Electrónica y Telecomunicaciones',
+                programa: 'TT - Tecnología en Telemática',
+                cursosCargados: 47,
+                estado: 'Cargado con éxito',
+                detalleCargue: [
+                    { nombre: 'Cálculo 1 Grupo A' },
+                    { nombre: 'Álgebra Lineal Grupo C' },
+                    // otros datos si es necesario
+                ],
+            },
+        });
+    }
+
 }
